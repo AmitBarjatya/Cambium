7fb21e94b1f2e5b0deb681c0fe546083f4eed43b
Amit Barjatya <lavibarjatya@gmail.com>
2017-11-04 17:26:05 +0530

    adding leak canary dependency

--- app/src/main/java/com/amit/cambium/App.java
+++ app/src/main/java/com/amit/cambium/App.java
@@ -3,6 +3,7 @@ package com.amit.cambium;
 import android.app.Application;
 
 import com.amit.cambium.utils.VolleySingleton;
+import com.squareup.leakcanary.LeakCanary;
 
 import io.realm.Realm;
 
@@ -20,6 +21,12 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         Realm.init(getApplicationContext());
         VolleySingleton.init(this);
     }
